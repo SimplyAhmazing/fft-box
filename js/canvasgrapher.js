e3db97bf70f372e4df2db533168d83420251c878
Ahmed Abdalla <aabdalla@amplify-nation.com>
2014-12-11 20:16:43 -0500

    refactors code; gets familiar with it again

--- js/canvasgrapher.js
+++ js/canvasgrapher.js
@@ -17,3 +17,39 @@ CanvasGrapher.prototype.graph = function(barArray){
 		this.ctx.fillRect(i * barWidth, this.height, barWidth - 2, -barHeight);
 	}
 };
+
+
+CanvasGrapher.prototype.graphColoredBoxes = function(data, xBoxes, yBoxes, randomizeBoxArrangement){
+	var totalBoxes = xBoxes * yBoxes;
+	var boxHeight = this.height / yBoxes;
+	var boxWidth = this.width / xBoxes;
+
+	function rainbow(n) {
+			return 'hsl(' + n + ',100%,50%)';
+	}
+
+	this.ctx.clearRect(0, 0, this.width, this.height);
+
+	// console.log('num boxes: ', xBoxes, yBoxes);
+	for (var r = 0; r < xBoxes; r++){
+		for ( var c = 0; c < yBoxes; c++){
+			var dataVal = data[r * yBoxes + c];
+			this.ctx.fillStyle = rainbow(dataVal); // fillStyleString;
+			this.ctx.lineWidth = 2;
+			this.ctx.strokeStyle = 'black';
+
+			this.ctx.strokeRect(
+				r * boxWidth,
+				c * boxHeight,
+				boxWidth,
+				boxHeight
+			);
+			this.ctx.fillRect(
+				r * boxWidth,
+				c * boxHeight,
+				boxWidth,
+				boxHeight
+			);
+		}
+	}
+};
--- js/remoteaudioplayer.js
+++ js/remoteaudioplayer.js
@@ -1,23 +1,31 @@
 RemoteAudioPlayer = function(context, url, afterLoad) {
     this.url = url;
     this.source = context.createBufferSource();
-    this.buffer = 0;
+    this.last_loaded_buffer = 0;
     this.context = context;
     this.afterLoad = afterLoad || this.ready;
 }
 
 RemoteAudioPlayer.prototype.ready = function(){
-    this.source.buffer = this.buffer;
+    this.source.buffer = this.last_loaded_buffer;
+    this.prepSource()
     // this.source.connect(this.context.destination);
-    analyzer = this.context.createAnalyser();
-    analyzer.fftSize = 2048;
-    this.source.connect(analyzer);
-    analyzer.connect(this.context.destination);
-    this.source.noteOn(0);
+    // analyzer = this.context.createAnalyser();
+    // analyzer.fftSize = 2048;
+    // this.source.connect(analyzer);
+    // analyzer.connect(this.context.destination);
+    this.source.start(0);
 };
 
 RemoteAudioPlayer.prototype.getSource = function() {
     return this.source;
+}
+
+RemoteAudioPlayer.prototype.prepSource = function(source){
+
+    debugger;
+    var sourceToPrep = source || this.source;
+    sourceToPrep.connect(this.context.destination);
 }
 
 RemoteAudioPlayer.prototype.load = function(callback) {
@@ -28,7 +36,7 @@ RemoteAudioPlayer.prototype.load = function(callback) {
     request.onload = function() {
         that.context.decodeAudioData(this.response, function(buffer){
             console.log('got to load sound file');
-            that.buffer = buffer;
+            that.last_loaded_buffer = buffer;
             that.afterLoad(that);
         });
     };
@@ -37,5 +45,19 @@ RemoteAudioPlayer.prototype.load = function(callback) {
 }
 
 RemoteAudioPlayer.prototype.reload = function(callback) {
-    this.source.buffer = this.buffer;
+    this.source = this.context.createBufferSource();
+    this.source.buffer = this.last_loaded_buffer;
+    this.prepSource(this.source);
+    return this.source;
+}
+
+RemoteAudioPlayer.prototype.stop = function(){
+    this.source.stop(0);
+    return true;
+}
+
+RemoteAudioPlayer.prototype.replay = function(){
+    var newSource = this.reload();
+    newSource.start(0);
+    return true;
 }
